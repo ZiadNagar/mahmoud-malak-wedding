50ba5c88a9b4975df11566e74882ab838ec910e4
Ziad Elnagar <57187405+ZiadNagar@users.noreply.github.com>
2026-05-10 12:59:48 +0300

    update

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
         padding: "80px 24px 80px",
       }}
     >
-      {/* Content Container - 480px max-width */}
+      {/* Content Container */}
       <div
         style={{
           maxWidth: "480px",
